c460f36bd8faec4fa523b50930ddc96fddf8d8b7
Austin Clyde <aclyde@uchicago.edu>
2019-10-06 09:57:20 -0500

    update

--- include/StreamingMoleculeReader/rdkit_interface.h
+++ include/StreamingMoleculeReader/rdkit_interface.h
@@ -15,20 +15,21 @@
 namespace SMR {
 
     boost::optional<std::string> getCannonicalSmileFromSmile(std::string const &smi) {
-        RDKit::ROMol *mol1;
+        RDKit::ROMol *mol1 = nullptr;
         try {
             mol1 = RDKit::SmilesToMol(smi);
         } catch (...) {
-//            if (mol1 != nullptr) {
-//                delete mol1;
-//            }
+            if (mol1 != nullptr) {
+                delete mol1;
+                mol1 = nullptr;
+            }
             return {};
         }
 
         if (mol1 != nullptr) {
             auto tmp = RDKit::MolToSmiles(*mol1);
-            //delete mol1;
-            //mol1 = nullptr;
+            delete mol1;
+            mol1 = nullptr;
             return tmp;
         } else {
             return {};
